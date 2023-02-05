5a02afa90aa86352edfb6e8a5402237303b9943c
ju1ius <jules.bernable@gmail.com>
2023-02-05 17:07:05 +0100

    extracts an interface for the PublicSuffixList class.

--- src/PublicSuffixList.php
+++ src/PublicSuffixList.php
@@ -2,25 +2,13 @@
 
 namespace ju1ius\FusBup;
 
-use ju1ius\FusBup\Exception\PrivateETLDException;
-use ju1ius\FusBup\Exception\UnknownTLDException;
 use ju1ius\FusBup\Loader\DafsaLoader;
 use ju1ius\FusBup\Loader\LoaderInterface;
 use ju1ius\FusBup\Lookup\LookupInterface;
 use ju1ius\FusBup\Utils\Idn;
 
-final class PublicSuffixList
+final class PublicSuffixList implements PublicSuffixListInterface
 {
-    public const FORBID_NONE = LookupInterface::FORBID_NONE;
-    /**
-     * Forbids private suffixes (not in the ICANN section of the public suffix list).
-     */
-    public const FORBID_PRIVATE = LookupInterface::FORBID_PRIVATE;
-    /**
-     * Allows unknown TLDs.
-     */
-    public const FORBID_UNKNOWN = LookupInterface::FORBID_UNKNOWN;
-
     private readonly LookupInterface $lookup;
 
     public function __construct(
@@ -28,35 +16,16 @@ final class PublicSuffixList
     ) {
     }
 
-    /**
-     * Returns whether the given domain is an effective TLD.
-     */
     public function isEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): bool
     {
         return $this->getLookup()->isEffectiveTLD($domain, $flags);
     }
 
-    /**
-     * Returns the effective TLD of a domain.
-     *
-     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
-     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
-     *                                of the public suffix list
-     */
     public function getEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): string
     {
         return Idn::toUnicode($this->getLookup()->getEffectiveTLD($domain, $flags));
     }
 
-    /**
-     * Splits a domain into it's private and effective TLD parts.
-     *
-     * @returns array{string, string}
-     *
-     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
-     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
-     *                                of the public suffix list
-     */
     public function splitEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): ?array
     {
         [$head, $tail] = $this->getLookup()->split($domain, $flags);
@@ -66,13 +35,6 @@ final class PublicSuffixList
         ];
     }
 
-    /**
-     * Returns the registrable part (AKA eTLD+1) of a domain.
-     *
-      *@throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
-     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
-     *                                of the public suffix list
-     */
     public function getRegistrableDomain(string $domain, int $flags = self::FORBID_NONE): ?string
     {
         [$head, $tail] = $this->getLookup()->split($domain, $flags);
@@ -83,13 +45,6 @@ final class PublicSuffixList
         return Idn::toUnicode($tail);
     }
 
-    /**
-     * Splits a domain into it's private and registrable parts.
-     *
-     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
-     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
-     *                                of the public suffix list
-     */
     public function splitRegistrableDomain(string $domain, int $flags = self::FORBID_NONE): ?array
     {
         [$head, $tail] = $this->getLookup()->split($domain, $flags);
---
+++ src/PublicSuffixListInterface.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace ju1ius\FusBup;
+
+use ju1ius\FusBup\Exception\PrivateETLDException;
+use ju1ius\FusBup\Exception\UnknownTLDException;
+use ju1ius\FusBup\Lookup\LookupInterface;
+
+interface PublicSuffixListInterface
+{
+    public const FORBID_NONE = LookupInterface::FORBID_NONE;
+
+    /**
+     * Forbids private suffixes (not in the ICANN section of the public suffix list).
+     */
+    public const FORBID_PRIVATE = LookupInterface::FORBID_PRIVATE;
+
+    /**
+     * Forbids unknown TLDs.
+     */
+    public const FORBID_UNKNOWN = LookupInterface::FORBID_UNKNOWN;
+
+    /**
+     * Returns whether the given domain is an effective TLD.
+     */
+    public function isEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): bool;
+
+    /**
+     * Returns the effective TLD of a domain.
+     *
+     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
+     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
+     *                              of the public suffix list
+     */
+    public function getEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): string;
+
+    /**
+     * Splits a domain into it's private and effective TLD parts.
+     *
+     * @returns array{string, string}
+     *
+     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
+     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
+     *                              of the public suffix list
+     */
+    public function splitEffectiveTLD(string $domain, int $flags = self::FORBID_NONE): ?array;
+
+    /**
+     * Returns the registrable part (AKA eTLD+1) of a domain.
+     *
+     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
+     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
+     *                              of the public suffix list
+     */
+    public function getRegistrableDomain(string $domain, int $flags = self::FORBID_NONE): ?string;
+
+    /**
+     * Splits a domain into it's private and registrable parts.
+     *
+     * @throws UnknownTLDException If FORBID_UNKNOWN flag is set and the TLD is not in the public suffix list.
+     * @throws PrivateETLDException If FORBID_PRIVATE flag is set and the effective TLD is not in the ICANN section
+     *                              of the public suffix list
+     */
+    public function splitRegistrableDomain(string $domain, int $flags = self::FORBID_NONE): ?array;
+
+    /**
+     * Implements the {@link https://httpwg.org/specs/rfc6265.html#cookie-domain RFC6265 algorithm}
+     * for matching a cookie domain against a request domain.
+     */
+    public function isCookieDomainAcceptable(string $requestDomain, string $cookieDomain): bool;
+}
