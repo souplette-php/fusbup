ae8e8ee4419b41e77f3dbc945fea4c4b073b79c3
ju1ius <jules.bernable@gmail.com>
2023-02-07 01:15:18 +0100

    adds IDN conversion flags specified by the whatwg/url spec

--- src/Utils/Idn.php
+++ src/Utils/Idn.php
@@ -4,8 +4,14 @@ namespace ju1ius\FusBup\Utils;
 
 use ju1ius\FusBup\Exception\IdnException;
 
+/**
+ * @link https://url.spec.whatwg.org/#idna
+ */
 final class Idn
 {
+    private const TO_ASCII_FLAGS = \IDNA_USE_STD3_RULES|\IDNA_CHECK_BIDI|\IDNA_NONTRANSITIONAL_TO_ASCII;
+    private const TO_UTF8_FLAGS = \IDNA_USE_STD3_RULES|\IDNA_CHECK_BIDI|\IDNA_NONTRANSITIONAL_TO_UNICODE;
+
     /**
      * @param string[]|string $domain
      * @return string
@@ -15,7 +21,7 @@ final class Idn
         if (\is_array($domain)) {
             $domain = implode('.', $domain);
         }
-        $idn = idn_to_ascii($domain, \IDNA_NONTRANSITIONAL_TO_ASCII, \INTL_IDNA_VARIANT_UTS46, $info);
+        $idn = idn_to_ascii($domain, self::TO_ASCII_FLAGS, \INTL_IDNA_VARIANT_UTS46, $info);
         if ($idn === false) {
             throw IdnException::toAscii($domain, $info);
         }
@@ -31,7 +37,7 @@ final class Idn
         if (\is_array($domain)) {
             $domain = implode('.', $domain);
         }
-        $idn = idn_to_utf8($domain, \IDNA_NONTRANSITIONAL_TO_UNICODE, \INTL_IDNA_VARIANT_UTS46, $info);
+        $idn = idn_to_utf8($domain, self::TO_UTF8_FLAGS, \INTL_IDNA_VARIANT_UTS46, $info);
         if ($idn === false) {
             throw IdnException::toUnicode($domain, $info);
         }
